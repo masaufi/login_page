b0a9f00a91e64c76f062bde7dc6c6a171981af63
masaufi <mamirul0897@gmail.com>
2024-08-07 21:22:58 +0800

    use port 3030

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = 3030;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
--- test/server.test.js
+++ test/server.test.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3030;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..')));
